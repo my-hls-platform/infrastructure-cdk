c8ca9bfb0e88e9b8526bad3bae42d20a79630ebc
Mykola Kashubyak <kashubyak514@gmail.com>
2026-07-01 14:49:09 +0300

    Refactor code structure for improved readability and maintainability

--- lib/infrastructure-cdk-stack.ts
+++ lib/infrastructure-cdk-stack.ts
@@ -1,16 +1,48 @@
-import * as cdk from 'aws-cdk-lib/core';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from 'aws-cdk-lib'
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront'
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins'
+import * as s3 from 'aws-cdk-lib/aws-s3'
+import { Construct } from 'constructs'
 
 export class InfrastructureCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+		super(scope, id, props)
 
-    // The code that defines your stack goes here
+		const rawBucket = new s3.Bucket(this, 'RawVideoBucket', {
+			removalPolicy: cdk.RemovalPolicy.DESTROY,
+			autoDeleteObjects: true,
+			cors: [
+				{
+					allowedMethods: [
+						s3.HttpMethods.GET,
+						s3.HttpMethods.PUT,
+						s3.HttpMethods.POST,
+						s3.HttpMethods.HEAD,
+					],
+					allowedOrigins: ['*'],
+					allowedHeaders: ['*'],
+				},
+			],
+		})
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfrastructureCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+		const processedBucket = new s3.Bucket(this, 'ProcessedVideoBucket', {
+			removalPolicy: cdk.RemovalPolicy.DESTROY,
+			autoDeleteObjects: true,
+		})
+
+		const distribution = new cloudfront.Distribution(this, 'VideoDistribution', {
+			defaultBehavior: {
+				origin: new origins.S3Origin(processedBucket),
+				viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+				allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD,
+				cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+			},
+		})
+
+		new cdk.CfnOutput(this, 'RawBucketName', { value: rawBucket.bucketName })
+		new cdk.CfnOutput(this, 'ProcessedBucketName', { value: processedBucket.bucketName })
+		new cdk.CfnOutput(this, 'CloudFrontUrl', {
+			value: `https://${distribution.distributionDomainName}`,
+		})
+	}
 }
