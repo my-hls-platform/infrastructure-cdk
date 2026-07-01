b84de7943be92887ed422b70587d4dbb765d24c8
Mykola Kashubyak <kashubyak514@gmail.com>
2026-07-01 15:26:19 +0300

    chore: add pnpm workspace configuration to allow builds for unrs-resolver

--- lib/infrastructure-cdk-stack.ts
+++ lib/infrastructure-cdk-stack.ts
@@ -1,7 +1,12 @@
 import * as cdk from 'aws-cdk-lib'
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront'
 import { S3BucketOrigin } from 'aws-cdk-lib/aws-cloudfront-origins'
+import * as events from 'aws-cdk-lib/aws-events'
+import * as targets from 'aws-cdk-lib/aws-events-targets'
+import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as event_sources from 'aws-cdk-lib/aws-lambda-event-sources'
 import * as s3 from 'aws-cdk-lib/aws-s3'
+import * as sqs from 'aws-cdk-lib/aws-sqs'
 import { Construct } from 'constructs'
 
 export class InfrastructureCdkStack extends cdk.Stack {
@@ -11,6 +16,7 @@ export class InfrastructureCdkStack extends cdk.Stack {
 		const rawBucket = new s3.Bucket(this, 'RawVideoBucket', {
 			removalPolicy: cdk.RemovalPolicy.DESTROY,
 			autoDeleteObjects: true,
+			eventBridgeEnabled: true,
 			cors: [
 				{
 					allowedMethods: [
@@ -39,10 +45,57 @@ export class InfrastructureCdkStack extends cdk.Stack {
 			},
 		})
 
+		const dlq = new sqs.Queue(this, 'TranscoderDLQ', {
+			retentionPeriod: cdk.Duration.days(14),
+		})
+
+		const transcoderQueue = new sqs.Queue(this, 'TranscoderQueue', {
+			visibilityTimeout: cdk.Duration.minutes(15),
+			deadLetterQueue: {
+				queue: dlq,
+				maxReceiveCount: 3,
+			},
+		})
+
+		const s3UploadRule = new events.Rule(this, 'S3UploadRule', {
+			eventPattern: {
+				source: ['aws.s3'],
+				detailType: ['Object Created'],
+				detail: {
+					bucket: {
+						name: [rawBucket.bucketName],
+					},
+				},
+			},
+		})
+		s3UploadRule.addTarget(new targets.SqsQueue(transcoderQueue))
+
+		const transcoderLambda = new lambda.Function(this, 'TranscoderLambda', {
+			runtime: lambda.Runtime.NODEJS_20_X,
+			handler: 'index.handler',
+			code: lambda.Code.fromInline(`
+        exports.handler = async (event) => {
+          console.log("Event received from SQS:", JSON.stringify(event, null, 2));
+          return { statusCode: 200, body: 'Dummy Lambda executed successfully!' };
+        };
+      `),
+			timeout: cdk.Duration.minutes(15),
+			memorySize: 2048,
+		})
+
+		rawBucket.grantWrite(transcoderLambda)
+		processedBucket.grantWrite(transcoderLambda)
+		transcoderLambda.addEventSource(
+			new event_sources.SqsEventSource(transcoderQueue, {
+				batchSize: 1,
+			}),
+		)
+
 		new cdk.CfnOutput(this, 'RawBucketName', { value: rawBucket.bucketName })
 		new cdk.CfnOutput(this, 'ProcessedBucketName', { value: processedBucket.bucketName })
 		new cdk.CfnOutput(this, 'CloudFrontUrl', {
 			value: `https://${distribution.distributionDomainName}`,
 		})
+    new cdk.CfnOutput(this, 'TranscoderLambdaName', { value: transcoderLambda.functionName });
 	}
 }
